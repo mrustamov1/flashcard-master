f92a14af100087b0cfd1462534d251fa20346cd0
mrustamov1 <mr.rusttamov@gamil.com>
2025-05-27 22:33:45 +0500

    small fix

--- packages/web/src/context/questions.context.tsx
+++ packages/web/src/context/questions.context.tsx
@@ -1,17 +1,17 @@
-// import { createContext, useContext, useState } from "react"
+import { createContext, useContext, useState } from "react"
 
-// const QuestionContext = createContext(null)
+const QuestionContext = createContext(null)
 
-// export function QuestionProvider({ children }) {
-//   const [questions, setQuestions] = useState([])
+export function QuestionProvider({ children }) {
+  const [questions, setQuestions] = useState([])
 
-//   return (
-//     <QuestionContext.Provider value={{ questions, setQuestions }}>
-//       {children}
-//     </QuestionContext.Provider>
-//   )
-// }
+  return (
+    <QuestionContext.Provider value={{ questions, setQuestions }}>
+      {children}
+    </QuestionContext.Provider>
+  )
+}
 
-// export function useQuestions() {
-//   return useContext(QuestionContext)
-// }
+export function useQuestions() {
+  return useContext(QuestionContext)
+}
